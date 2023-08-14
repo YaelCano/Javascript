6c3bebccc15543bd0fd9792b159cab0f09918535
ApolMo2-056 <78746517+YaelCano@users.noreply.github.com>
2023-08-14 12:51:48 -0500

    a

---
+++ ejercicio-1.js
@@ -0,0 +1,20 @@
+//##################################
+//Ejercio 1
+//################################
+
+//Desarrollado por : Yael Mauricio Cano Palacio
+
+function calcular(edad) {
+    actual = 2023
+    final = 2023-edad
+    console.log("El año en el que naciste fue:"+edad);
+}
+calcular()
+
+//Ejercio 2#######
+function Celsiustofahrenheit(celsius) {
+    fahrenheit = 32 + (celsius*9/5)
+    console.log("La temperatura en Fahrenheit es: "+fahrenheit)
+    return fahrenheit
+}
+Celsiustofahrenheit(60)
